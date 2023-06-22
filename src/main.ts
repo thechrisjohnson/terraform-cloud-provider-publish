13db81292cb6e4d43b89aa0c7457cbd470e2f4e7
Christopher Johnson <Christopher.Johnson2@citrix.com>
2023-06-22 16:38:33 -0400

    Add code for the action.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,143 @@
+import * as fs from 'fs/promises'
+import * as path from 'path'
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as exec from '@actions/exec'
+import {TerraformClient, TerraformManifestFile} from './terraform'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    // Load configuration
+    const organizationName: string = core.getInput('organization-name')
+    const organizationKey: string = core.getInput('organization-key')
+    const providerDir: string = core.getInput('provider-directory')
+    const gpgKey: string = core.getInput('gpg-key')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // Create the terraform client
+    const tfClient = new TerraformClient(organizationName, organizationKey)
 
-    core.setOutput('time', new Date().toTimeString())
+    // Find the *manifest.json file, and calculate the required values from there
+    const providerFiles = await fs.readdir(providerDir)
+    const manifestFile = providerFiles.find(value => {
+      value.endsWith('manifest.json')
+    })
+    if (manifestFile === undefined) {
+      throw new Error(`Unable to find manifest file in ${providerDir}`)
+    }
+
+    const parts = manifestFile.split('_')
+    if (parts.length !== 3) {
+      throw new Error(`Invalid manifest file ${manifestFile}`)
+    }
+
+    const providerName = parts[0]
+    const providerVersion = parts[1]
+
+    // Read the last bit of information from the manifest file
+    const manifestRaw = await fs.readFile(path.join(providerDir, manifestFile))
+    const manifest: TerraformManifestFile = JSON.parse(manifestRaw.toString())
+    const providerProtocols = manifest.metadata.protocol_versions
+
+    // Now that we have the values we need, create everything
+    core.info(
+      `Checking to see if provider ${organizationName}/${providerName} already exists...`
+    )
+    let provider = await tfClient.getProvider(providerName)
+    if (provider == null) {
+      core.info(
+        `Provider did not exist, creating ${organizationName}/${providerName}...`
+      )
+      provider = await tfClient.postProvider(providerName)
+    }
+
+    core.info(`Ensuring gpg key exists...`)
+    const signingKey = await tfClient.postSingingKey(gpgKey)
+
+    core.info(`Creating new provider version ${providerVersion}`)
+    const version = await tfClient.postProviderVersion(
+      providerName,
+      providerVersion,
+      providerProtocols,
+      signingKey.id
+    )
+
+    core.info(`Uploading sha256 and sig files...`)
+    // Take the output folder for all of the files and look for a SHA256SUM and SHA256SUM.sig
+    const sumFileBase = providerFiles.find(value => {
+      value.endsWith('SHA256SUMS')
+    })
+    const signatureFileBase = providerFiles.find(value => {
+      value.endsWith('SHA256SUMS.sig')
+    })
+    if (sumFileBase === undefined || signatureFileBase === undefined) {
+      throw new Error('Unable to find sum file and/or signature file')
+    }
+    const sumFile = path.join(providerDir, sumFileBase)
+    const signatureFile = path.join(providerDir, signatureFileBase)
+
+    await uploadFile(version.links['shasums-upload'], sumFile)
+    await uploadFile(version.links['shasums-sig-upload'], signatureFile)
+
+    // Read the shasums file and upload platforms based on that
+    const platformsBuffer = await fs.readFile(sumFile)
+    const platforms = platformsBuffer.toString()
+    for (const line of platforms.split('\n')) {
+      const lineParts = line.split(' ')
+      if (lineParts.length !== 2) {
+        throw new Error(`Invalid sha256sums file ${sumFile}`)
+      }
+      const shasum = lineParts[0]
+      const file = lineParts[1]
+
+      const fileParts = file.split('_')
+      if (
+        fileParts.length !== 4 ||
+        fileParts[0] !== providerName ||
+        fileParts[1] !== providerVersion
+      ) {
+        core.debug(`Skipping file ${file}`)
+        continue
+      }
+
+      const os = fileParts[2]
+      const arch = fileParts[3]
+
+      core.info(
+        `Creating platform ${os}_${arch} for ${providerName} ${providerVersion}`
+      )
+      const platform = await tfClient.postProviderPlatform(
+        providerName,
+        providerVersion,
+        os,
+        arch,
+        shasum,
+        file
+      )
+      await uploadFile(
+        platform.links['provider-binary-upload'],
+        path.join(providerDir, file)
+      )
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+async function uploadFile(url: string, filePath: string): Promise<void> {
+  let error = ''
+  const options = {
+    listeners: {
+      stderr: (data: Buffer) => {
+        error += data.toString()
+      }
+    }
+  }
+
+  // This is just laziness, as I didn't want to write a multipart uploader using basic node
+  core.debug(`Uploading file: ${filePath}`)
+  await exec.exec('curl', ['-T', filePath, url], options)
+
+  if (error !== '') {
+    throw new Error(error)
   }
 }
 
---
+++ src/terraform.ts
@@ -0,0 +1,267 @@
+import {HttpClient} from '@actions/http-client'
+import {BearerCredentialHandler} from '@actions/http-client/lib/auth'
+
+function GenerateGetProviderUrl(
+  organizationName: string,
+  providerName: string
+): string {
+  return `https://app.terraform.io/organizations/${organizationName}/registry-providers/private/${organizationName}/${providerName}`
+}
+
+function GeneratePostProviderUrl(organizationName: string): string {
+  return `https://app.terraform.io/organizations/${organizationName}/registry-providers`
+}
+
+function GeneratePostProviderVersionUrl(
+  organizationName: string,
+  providerName: string
+): string {
+  return `https://app.terraform.io/organizations/${organizationName}/registry-providers/private/${organizationName}/${providerName}/versions`
+}
+
+function GeneratePostProviderPlatformUrl(
+  organizationName: string,
+  providerName: string,
+  version: string
+): string {
+  return `https://app.terraform.io/organizations/${organizationName}/registry-providers/private/${organizationName}/${providerName}/versions/${version}/platforms`
+}
+
+export class TerraformClient {
+  organizationName: string
+  httpClient: HttpClient
+
+  constructor(organizationName: string, organizationKey: string) {
+    this.organizationName = organizationName
+    const bearerHandler = new BearerCredentialHandler(organizationKey)
+    this.httpClient = new HttpClient('Publish Private Provider Action', [
+      bearerHandler
+    ])
+  }
+
+  async getProvider(providerName: string): Promise<TerraformProvider | null> {
+    const response = await this.httpClient.getJson<
+      DataWrappedValue<TerraformProvider>
+    >(GenerateGetProviderUrl(this.organizationName, providerName))
+
+    return response.result?.data ?? null
+  }
+
+  async postProvider(providerName: string): Promise<TerraformProvider> {
+    const body = {
+      data: {
+        type: 'registry-providers',
+        attributes: {
+          name: providerName,
+          namespace: this.organizationName,
+          'registry-name': 'private'
+        }
+      }
+    }
+
+    const response = await this.httpClient.postJson<
+      DataWrappedValue<TerraformProvider>
+    >(GeneratePostProviderUrl(this.organizationName), body)
+    if (response.result == null) {
+      throw new Error(`Invalid reponse code: ${response.statusCode}`)
+    }
+
+    return response.result.data
+  }
+
+  async postSingingKey(asciiArmor: string): Promise<TerraformSigningKey> {
+    const body = {
+      data: {
+        type: 'gpg-keys',
+        attributes: {
+          namespace: this.organizationName,
+          'ascii-armor': asciiArmor
+        }
+      }
+    }
+
+    const response = await this.httpClient.postJson<
+      DataWrappedValue<TerraformSigningKey>
+    >('https://app.terraform.io/api/registry/private/v2/gpg-keys', body)
+    if (response.result == null) {
+      throw new Error(`Invalid response code: ${response.statusCode}`)
+    }
+
+    return response.result.data
+  }
+
+  async postProviderVersion(
+    providerName: string,
+    version: string,
+    supportedProtocols: string[],
+    keyId: string
+  ): Promise<TerraformProviderVersion> {
+    const body = {
+      data: {
+        type: 'registry-provider-versions',
+        version,
+        'key-id': keyId,
+        protocols: supportedProtocols
+      }
+    }
+
+    const response = await this.httpClient.postJson<
+      DataWrappedValue<TerraformProviderVersion>
+    >(GeneratePostProviderVersionUrl(this.organizationName, providerName), body)
+    if (response.result == null) {
+      throw new Error(`Invalid response code: ${response.statusCode}`)
+    }
+
+    return response.result.data
+  }
+
+  async postProviderPlatform(
+    providerName: string,
+    version: string,
+    os: string,
+    arch: string,
+    shasum: string,
+    filename: string
+  ): Promise<TerraformProviderPlatform> {
+    const body = {
+      data: {
+        type: 'registry-provider-version-platforms',
+        attributes: {
+          os,
+          arch,
+          shasum,
+          filename
+        }
+      }
+    }
+
+    const response = await this.httpClient.postJson<
+      DataWrappedValue<TerraformProviderPlatform>
+    >(
+      GeneratePostProviderPlatformUrl(
+        this.organizationName,
+        providerName,
+        version
+      ),
+      body
+    )
+    if (response.result == null) {
+      throw new Error(`Invalid response code: ${response.statusCode}`)
+    }
+
+    return response.result.data
+  }
+}
+
+export interface DataWrappedValue<T> {
+  data: T
+}
+
+export interface TerraformProvider {
+  id: string
+  type: string
+  attributes: TerraformProviderAttributes
+  relationships: TerraformProviderRelationships
+  links: TerraformLinks
+}
+
+export interface TerraformProviderAttributes {
+  name: string
+  namespace: string
+  'registry-name': string
+  'created-at': string
+  'updated-at': string
+  permissions: TerraformProviderPermissions
+}
+
+export interface TerraformProviderPermissions {
+  'can-delete': boolean
+  'can-upload-asset': boolean | undefined
+}
+
+export interface TerraformProviderRelationships {
+  versions: TerraformProviderVersions
+}
+
+export interface TerraformProviderVersions {
+  data: TerraformProviderVersions[]
+}
+
+export interface TerraformProviderVersionReference {
+  id: string
+  type: string
+}
+
+export interface TerraformLinks {
+  self: string
+}
+
+export interface TerraformSigningKey {
+  type: string
+  id: string
+  attributes: TerraformSigningKeyAttributes
+  links: TerraformLinks
+}
+
+export interface TerraformSigningKeyAttributes {
+  'ascii-armor': string
+  'created-at': string
+  'key-id': string
+  namespace: string
+  source: string
+  'source-url': string
+  'trust-signature': string
+  'updated-at': string
+}
+
+export interface TerraformProviderVersion {
+  id: string
+  type: string
+  attributes: TerraformProviderVersionAttributes
+  links: TerraformProviderVersionLinks
+}
+
+export interface TerraformProviderVersionAttributes {
+  version: string
+  'created-at': string
+  'updated-at': string
+  'key-id': string
+  protocols: string[]
+  permissions: TerraformProviderPermissions
+  'shasums-uploaded': boolean
+  'shasums-sig-uploaded': boolean
+}
+
+export interface TerraformProviderVersionLinks {
+  'shasums-upload': string
+  'shasums-sig-upload': string
+}
+
+export interface TerraformProviderPlatform {
+  id: string
+  type: string
+  attributes: TerraformProviderPlatformAttributes
+  links: TerraformProviderPlatformLinks
+}
+
+export interface TerraformProviderPlatformAttributes {
+  os: string
+  arch: string
+  shasum: string
+  filename: string
+  permissions: TerraformProviderPermissions
+  'provider-binary-uploaded': boolean
+}
+
+export interface TerraformProviderPlatformLinks {
+  'provider-binary-upload': string
+}
+
+export interface TerraformManifestFile {
+  version: number
+  metadata: TerraformManifestFileMetadata
+}
+
+export interface TerraformManifestFileMetadata {
+  protocol_versions: string[]
+}
